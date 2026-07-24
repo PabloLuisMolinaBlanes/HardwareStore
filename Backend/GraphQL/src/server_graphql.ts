182ae595667a2b6de4521596cb0f1bc5029f7201
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-07-24 16:41:58 +0200

    Agregado esqueleto de tfidf y agregado servidor basico GraphQL

---
+++ Backend/GraphQL/src/server_graphql.ts
@@ -0,0 +1,53 @@
+/*Code modified from https://www.apollographql.com/docs/apollo-server/getting-started */
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+
+  type Test {
+    test1: String
+    test2: String
+  }
+
+  type Query {
+    tests: [Test]
+  }
+`;
+
+const tests = [
+  {
+    test1: 'Test1',
+    test2: 'Test2',
+  },
+  {
+    test1: 'Test1',
+    test2: 'Test2',
+  },
+];
+
+const resolvers = {
+  Query: {
+    tests: () => tests,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at: ${url}`);
---
+++ Backend/MicroServices/ContentBasedRetrieval/tfidf.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from nltk.corpus import stopwords
+from nltk import ngrams
+import re
+import pandas as pd
+import pprint
+import sys
+
+cachedStopWords = stopwords.words("english")
+cachedStopWords.extend(stopwords.words("spanish"))
+ngrams_min = 1
+ngrams_max = 1
+min_df = 1
+
+def turnToTfIdfModel(allsentences: list[str]):
+    vectorizer = TfidfVectorizer(ngram_range=(ngrams_min,ngrams_max), min_df=min_df)
+    X = vectorizer.fit_transform(allsentences)
+    doc_term_matrix = X.todense()
+    df = pd.DataFrame(
+        doc_term_matrix,
+        columns=vectorizer.get_feature_names_out(),
+        index=range(len(allsentences)),
+    )
+    return df
+
+def normalize_string(s) -> str:
+    s : str = re.sub(r"\n{1,}", " ", s)
+    s : str = re.sub(r"^ *", "", s)
+    s : str = re.sub(r"[\(\[].*?[\)\]]", "", s)
+    s: str = re.sub(r"<br>", "", s)
+    s: str = re.sub(r"&gt;&gt;", "", s)
+    s = s.lower()
+    s = ''.join(e for e in s if e.isalnum() or e.isspace())
+    s = ' '.join([word for word in s.split() if word not in cachedStopWords])
+    return s
